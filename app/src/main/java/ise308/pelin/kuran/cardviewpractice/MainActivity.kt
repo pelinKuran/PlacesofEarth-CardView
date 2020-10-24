5072a443fd8454f67cfc7d595a78e05809d0c4e7
Pelin Rabia Kuran <pelinrkuran@gmail.com>
2020-10-24 14:14:02 +0300

    first version

---
+++ app/src/main/java/ise308/pelin/kuran/cardviewpractice/MainActivity.kt
@@ -0,0 +1,12 @@
+package ise308.pelin.kuran.cardviewpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scroll_layout)
+    }
+}
